de3d86870b9902cc7171a249c6163876759a7ec0
Johan Andersson <johan.peter.andersson@gmail.com>
2021-12-05 19:28:16 +0100

    Day05 part 2

--- day05/day05.py
+++ day05/day05.py
@@ -53,5 +53,56 @@ def part1(file):
     print("Part 1 for {}:".format(file))
     print(points)
 
+def part2(file):
+    lines = [l for l in get_lines(file)]
+    marks = {}
+    for line in lines:
+        x0,y0 = line['src']
+        x1,y1 = line['dest']
+
+        if not non_diagonal(line):
+            diagonal = True
+        else:
+            diagonal = False
+
+        if x0 < x1:
+            xdiff = 1
+        elif x0 > x1:
+            xdiff = -1
+        elif x0 == x1:
+            xdiff = 0
+
+        if y0 < y1:
+            ydiff = 1
+        elif y0 > y1:
+            ydiff = -1
+        elif y0 == y1:
+            ydiff = 0
+
+        xend = x1 + xdiff
+        yend = y1 + ydiff
+
+        x = x0
+        y = y0
+
+        while x != xend or y != yend:
+            if (x,y) in marks:
+                marks[(x,y)] += 1
+            else:
+                marks[(x,y)] = 1
+            y += ydiff
+            x += xdiff
+
+    points = 0
+    for coord in marks:
+        if marks[coord] > 1:
+            points += 1
+    print("Part 2 for {}:".format(file))
+    print(points)
+
 part1('test.txt')
 part1('input.txt')
+print('')
+
+part2('test.txt')
+part2('input.txt')

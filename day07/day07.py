ae9dd47da2b8aff6e2c493db71fe1e9b2de5e605
Johan Andersson <johan.peter.andersson@gmail.com>
2021-12-07 11:28:49 +0100

    Day 07 part 1

---
+++ day07/day07.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+def get_initial_nbrs(filename):
+    all_nbrs = []
+    with open(filename, 'r') as file:
+        all_nbrs = [int(n.strip()) for n in file.readline().split(',')]
+
+    nbrs = defaultdict(lambda: 0)
+    for nbr in all_nbrs:
+        nbrs[nbr] += 1
+
+    return nbrs
+
+def get_cost(positions, current_pos):
+    total = 0
+    for pos in positions:
+        if pos != current_pos:
+            total += positions[pos] * abs(pos - current_pos)
+    return total
+
+def part1(filename):
+    positions = get_initial_nbrs(filename)
+    lowest_cost = 10000000
+    best_pos = -1
+    for pos in positions:
+        cost = get_cost(positions, pos)
+        if cost < lowest_cost:
+            lowest_cost = cost
+            best_pos = pos
+    print("Part 1 for {}: Best pos: {}, cost: {}".format(filename, best_pos, lowest_cost))
+
+
+
+part1('test.txt')
+part1('input.txt')
+print('')

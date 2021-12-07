4d0ebed8db564e5fe4f5b56313ca0567df6a4a5b
Johan Andersson <johan.peter.andersson@gmail.com>
2021-12-07 11:44:35 +0100

    Day 07 part 2

--- day07/day07.py
+++ day07/day07.py
@@ -30,7 +30,43 @@ def part1(filename):
     print("Part 1 for {}: Best pos: {}, cost: {}".format(filename, best_pos, lowest_cost))
 
 
+cache = {}
+
+def integral(n):
+    if n in cache:
+        return cache[n]
+    i = 1
+    result = 0
+    while i <= n:
+        result += i
+        i += 1
+    cache[n] = result
+    return result
+
+def get_cost2(positions, current_pos):
+    total = 0
+    for pos in positions:
+        if pos != current_pos:
+            total += positions[pos] * integral(abs(pos - current_pos))
+    return total
+
+def part2(filename):
+    positions = get_initial_nbrs(filename)
+    lowest_cost = -1
+    best_pos = -1
+    for pos in range(0, max(positions)):
+        cost = get_cost2(positions, pos)
+        if lowest_cost == -1 or cost < lowest_cost:
+            lowest_cost = cost
+            best_pos = pos
+    print("Part 2 for {}: Best pos: {}, cost: {}".format(filename, best_pos, lowest_cost))
+
+
 
 part1('test.txt')
 part1('input.txt')
+print('')
+
+part2('test.txt')
+part2('input.txt')
 print('')

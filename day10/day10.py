ba9b9eb150c3f2a6745dd5ed79bef2a19dd042f9
Johan Andersson <johan.peter.andersson@gmail.com>
2021-12-10 09:05:53 +0100

    Day 10 part 2

--- day10/day10.py
+++ day10/day10.py
@@ -28,7 +28,6 @@ def part1(filename):
     stack = deque()
     total = 0
     for line in lines:
-        print(line)
         for c in line:
             if c in openers:
                 stack.append(c)
@@ -38,10 +37,52 @@ def part1(filename):
             elif c == matching[stack[-1]]:
                 stack.pop()
             else:
-                print('Expected ' + matching[stack[-1]] + ' but got ' + c)
                 total += values[c]
                 break
 
     print('Part 1 for {}: {}'.format(filename, total))
 
+
+closer_scores = {
+        '(': 1,
+        '[': 2,
+        '{': 3,
+        '<': 4,
+        }
+
+def part2(filename):
+    lines = get_input(filename)
+
+    scores = []
+    for line in lines:
+        stack = deque()
+        corrupt = False
+        for c in line:
+            if c in openers:
+                stack.append(c)
+            elif len(stack) == 0:
+                corrupt = True
+                break
+            elif c == matching[stack[-1]]:
+                stack.pop()
+            else:
+                corrupt = True
+                break
+
+        if not corrupt:
+            score = 0
+            while len(stack) > 0:
+                score *= 5
+                c = stack.pop()
+                score += closer_scores[c]
+            scores.append(score)
+    scores = sorted(scores)
+    winner = scores[int(len(scores) / 2)]
+    print('Part 2 for {}: {}'.format(filename, winner))
+
+part1('test.txt')
 part1('input.txt')
+print('')
+
+part2('test.txt')
+part2('input.txt')

9df9342dc5aa3171d22e22af617eb0f5137a065c
Johan Andersson <johan.peter.andersson@gmail.com>
2021-12-17 10:12:17 +0100

    Day17 remove debug code and prints

--- day17/day17.py
+++ day17/day17.py
@@ -1,10 +1,3 @@
-from collections import defaultdict, deque
-from queue import PriorityQueue
-import sys
-import math
-
-
-# target area: x=206..250, y=-105..-57
 def get_input(filename):
     line = ""
     with open(filename, "r") as file:
@@ -35,11 +28,9 @@ def shoot(v0, target):
     hit = False
     closest_y = 1000000
 
-    while not overshot(target, (xpos, ypos)) and not in_target(target, (xpos, ypos)):
+    while not overshot(target,
+                       (xpos, ypos)) and not in_target(target, (xpos, ypos)):
         (x0, x1, y0, y1) = target
-
-        if (v0 == (6, 0)) or (v0 == (7, -1)):
-            print("pos", xpos, ypos, "vel:", vx, vy)
 
         xpos += vx
         ypos += vy
@@ -51,8 +42,6 @@ def shoot(v0, target):
         maxy = max(maxy, ypos)
         if in_target(target, (xpos, ypos)):
             hit = True
-            if (v0 == (6, 0)) or (v0 == (7, -1)):
-                print("hit")
 
         if ypos < y0:
             ydiff = y0 - ypos
@@ -62,10 +51,6 @@ def shoot(v0, target):
             ydiff = 0
         closest_y = min(closest_y, ydiff)
 
-        if (v0 == (6, 0)) or (v0 == (7, -1)):
-            print("overshot?", target, overshot(target, (xpos, ypos)))
-
-    #print(v0, hit, closest_y)
     return hit, maxy, closest_y
 
 
@@ -74,19 +59,15 @@ def part1(filename):
     (x0, target_xmax, y0, y1) = target
     total_maxy = -1
     vy = 0
-    xdiff = 1
     hit = False
-
     closest_y = 0
     while closest_y < 200:
         vx = 1
         for vx in range(1, target_xmax + 1):
             hit, maxy, closest_y = shoot((vx, vy), target)
             if hit:
-                #print(vx,vy)
                 total_maxy = max(total_maxy, maxy)
         vy += 1
-        #print(total_maxy, closest_y)
 
     return total_maxy
 
@@ -94,47 +75,34 @@ def part1(filename):
 def part2(filename):
     target = get_input(filename)
     (x0, target_xmax, y0, y1) = target
-    vy = 0
-    xdiff = 1
     hit = False
     closest_y = 0
-    nbr_hits = 0
     hits = set()
     ydiff = 1
     maxdiff = 200
+    vy = 0
     while abs(closest_y) < maxdiff or ydiff == 1:
         for vx in range(0, target_xmax + 2):
             hit, maxy, closest_y = shoot((vx, vy), target)
             if hit:
-                nbr_hits += 1
                 hits.add((vx, vy))
-                #print(vx,vy)
-            # if vx == 6 and vy == 0 or vx == 7 and vy == -1:
-            #     print(vx, vy, closest_y, hit)
         vy += ydiff
-        #print(total_maxy, closest_y)
         if abs(closest_y) >= maxdiff:
             if ydiff == -1:
                 break
             closest_y = 0
             vy = 0
             ydiff = -1
-            print("switcheroo")
 
-    # for hit in sorted(hits):
-    #     print("{},{}".format(hit[0],hit[1]))
-
-    print(len(hits))
     return len(hits)
 
 
-#missing:
-#< 6,0
-#< 7,-1
+assert part1("test.txt") == 45
+result1 = part1("input.txt")
+assert result1 == 5460
+print("Part 1: {}".format(result1))
 
-#print(part1("test.txt"))
-#print(part1("input.txt"))
-# result1 = part1("input.txt")
-# print("Part 1: {}".format(result1))
-
-print(part2("input.txt"))
+assert part2("test.txt") == 112
+result2 = part2("input.txt")
+assert result2 == 3618
+print("Part 2: {}".format(result2))

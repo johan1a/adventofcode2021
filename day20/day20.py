3a99b4e3883842e2577f57c1d08c927ca482f7c8
Johan Andersson <johan.peter.andersson@gmail.com>
2021-12-20 11:35:56 +0100

    day20 part 1

---
+++ day20/day20.py
@@ -0,0 +1,96 @@
+from collections import defaultdict
+import math
+import pprint
+pp = pprint.PrettyPrinter(indent=2)
+
+
+def get_input(filename):
+    lines = []
+    with open(filename, "r") as file:
+        lines = [l.strip() for l in file.readlines()]
+
+    image = defaultdict(lambda: defaultdict(lambda: '.'))
+    for i in range(2, len(lines)):
+      for j in range(0, len(lines[i])):
+        image[i - 2][j] = lines[i][j]
+
+    return lines[0], image, 0, 0, len(image[2])-1, len(image) -1
+
+def pixel_to_binary(char):
+  if char == '.':
+    return '0'
+  else:
+    return '1'
+
+
+def get_image_number(input_image, x0, y0):
+  digits = []
+  for y in range(y0-1, y0+2):
+    for x in range(x0-1, x0+2):
+      digits.append(input_image[y][x])
+  return int("".join([pixel_to_binary(x) for x in digits]), 2)
+
+border = 20
+
+def enhance(enhancement, input_image, xmin, ymin, xmax, ymax):
+  default = enhancement[get_image_number(input_image, -100000, -100000)]
+
+  output_image = defaultdict(lambda: defaultdict(lambda: default))
+
+  next_xmin = xmin
+  next_ymin = ymin
+  next_xmax = xmax
+  next_ymax = ymax
+
+  for y in range(ymin-border, ymax+1 + border):
+    for x in range(xmin-border, xmax+ 1+ border):
+      num = get_image_number(input_image, x, y)
+      output_pixel = enhancement[num]
+      if y == -1 and x == -1:
+        print('num',num, x,y, 'output_pixel',output_pixel)
+      output_image[y][x] = output_pixel
+
+      next_xmin = min(next_xmin, x)
+      next_ymin = min(next_ymin, y)
+      next_xmax = max(next_xmax, x)
+      next_ymax = max(next_ymax, y)
+
+  return output_image, next_xmin -3 , next_ymin -3, next_xmax +3, next_ymax+3
+
+def print_image(output_image, xmin, ymin, xmax, ymax):
+  for y in range(ymin, ymax+1):
+    for x in range(xmin, xmax+1):
+      print(output_image[y][x], end="")
+    print()
+  print()
+
+
+def part1(filename):
+  enhancement, input_image, xmin, ymin, xmax, ymax = get_input(filename)
+  #print(xmin, ymin, xmax, ymax)
+  #print_image(input_image, xmin, ymin, xmax, ymax)
+  image = input_image
+  n = 2
+  for i in range(0,n):
+    image, xmin, ymin, xmax, ymax = enhance(enhancement, image, xmin, ymin, xmax, ymax)
+    xmin += border
+    ymin += border
+    xmax -= border
+    ymax -= border
+    print(xmin, ymin, xmax, ymax)
+    print_image(image, xmin, ymin, xmax, ymax)
+
+
+  total = 0
+  for row in image:
+    for col in image:
+      if image[row][col] == '#':
+        total += 1
+
+  return total
+
+print(part1('input.txt'))
+#print(part1('test.txt'))
+# result1 = part1("input.txt")
+# assert result1 == 472
+# print("Part 1: {}".format(result1))
